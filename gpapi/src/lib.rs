01056c9707cc1f6024d0d409828b96eee2e4141e
Jakob <git@themoep.at>
2025-03-17 15:04:09 +0100

    added support for runtime device.properties inclusion

--- gpapi/src/lib.rs
+++ gpapi/src/lib.rs
@@ -71,6 +71,7 @@ use futures::future::TryFutureExt;
 //use hyper::header::{HeaderName as HyperHeaderName, HeaderValue as HyperHeaderValue};
 //use hyper::{Body, Client, Method, Request};
 //use hyper_tls::HttpsConnector;
+use configparser::ini::Ini;
 use prost::Message;
 use reqwest::header::{HeaderMap, HeaderValue, HeaderName};
 use reqwest::Url;
@@ -79,17 +80,7 @@ use tokio_dl_stream_to_disk::AsyncDownload;
 use crate::error::{Error as GpapiError, ErrorKind as GpapiErrorKind};
 
 use googleplay_protobuf::{
-    AcceptTosResponse,
-    AndroidCheckinProto,
-    AndroidCheckinRequest,
-    AndroidCheckinResponse,
-    BulkDetailsRequest,
-    BulkDetailsResponse,
-    DetailsResponse,
-    DeviceConfigurationProto,
-    ResponseWrapper,
-    UploadDeviceConfigRequest,
-    UploadDeviceConfigResponse,
+    AcceptTosResponse, AndroidBuildProto, AndroidCheckinProto, AndroidCheckinRequest, AndroidCheckinResponse, BulkDetailsRequest, BulkDetailsResponse, DetailsResponse, DeviceConfigurationProto, DeviceFeature, ResponseWrapper, UploadDeviceConfigRequest, UploadDeviceConfigResponse
 };
 
 use serde::{Serialize, Deserialize};
@@ -148,6 +139,29 @@ impl Gpapi {
             //hyper_client: Box::new(hyper_client),
         }
     }
+
+    pub fn with_extra_device_properties<S: Into<String>>(device_codename: S, email: S, extra_device_properties_path: &Path) -> Self { 
+
+        let _device_properties = read_device_properties_from_file(extra_device_properties_path, device_codename);
+
+        Gpapi {
+            locale: String::from("en_US"),
+            timezone: String::from("UTC"),
+            device_properties: _device_properties,
+            email: email.into(),
+            aas_token: None,
+            auth_token: None,
+            device_config_token: None,
+            device_checkin_consistency_token: None,
+            tos_token: None,
+            dfe_cookie: None,
+            gsf_id: None,
+            client: Box::new(reqwest::Client::new()),
+            //hyper_client: Box::new(hyper_client),
+        }
+    }
+
+
 
     /// Set the locale
     pub fn set_locale<S: Into<String>>(&mut self, locale: S){
@@ -1102,6 +1116,147 @@ impl BuildConfiguration {
     }
 }
 
+fn read_device_properties_from_file<S: Into<String>>(device_properties_path: &Path, device_codename: S) -> DeviceProperties {
+    let mut config = Ini::new();
+    config
+        .read(fs::read_to_string(device_properties_path).expect("Can't read custom device.properties file."))
+        .expect("Can't parse custom device.properties file.");
+
+    let device_codename_str = device_codename.into();
+
+    for section in config.sections() {
+        println!("...reading '{:?}', looking for '{}'", section, device_codename_str);  
+        if section.replace("\"", "") == device_codename_str {
+            println!("......found!");
+            let mut extra_info = HashMap::new();
+            extra_info.insert("Build.ID".to_string(), config.get(&section, "Build.ID").unwrap_or_default());
+            extra_info.insert("Vending.versionString".to_string(), config.get(&section, "Vending.versionString").unwrap_or_default());
+            extra_info.insert("Vending.version".to_string(), config.get(&section, "Vending.version").unwrap_or_default());
+            extra_info.insert("Build.VERSION.RELEASE".to_string(), config.get(&section, "Build.VERSION.RELEASE").unwrap_or_default());
+            if let Some(sim_operator) = config.get(&section, "SimOperator") {
+                extra_info.insert("SimOperator".to_string(), sim_operator);
+            }
+            let mut android_build = AndroidBuildProto::default();
+            android_build.id = config.get(&section, "Build.FINGERPRINT");
+            android_build.product = config.get(&section, "Build.HARDWARE");
+            android_build.carrier = config.get(&section, "Build.BRAND");
+            android_build.radio = config.get(&section, "Build.RADIO");
+            android_build.bootloader = config.get(&section, "Build.BOOTLOADER");
+            android_build.device = config.get(&section, "Build.DEVICE");
+            android_build.sdk_version = config
+                .getint(&section, "Build.VERSION.SDK_INT")
+                .unwrap()
+                .map(|v| v as i32);
+            android_build.model = config.get(&section, "Build.MODEL");
+            android_build.manufacturer = config.get(&section, "Build.MANUFACTURER");
+            android_build.build_product = config.get(&section, "Build.PRODUCT");
+            android_build.client = config.get(&section, "Client");
+            android_build.ota_installed = Some(false);
+            android_build.google_services = config
+                .getint(&section, "GSF.version")
+                .unwrap()
+                .map(|v| v as i32);
+            let mut android_checkin = AndroidCheckinProto::default();
+            android_checkin.build = Some(android_build);
+            android_checkin.last_checkin_msec = Some(0);
+            android_checkin.cell_operator =
+                config.get(&section, "CellOperator");
+            android_checkin.sim_operator = config.get(&section, "SimOperator");
+            android_checkin.roaming = config.get(&section, "Roaming");
+            android_checkin.user_number = Some(0);
+            
+            let mut device_configuration = DeviceConfigurationProto::default();
+            device_configuration.touch_screen = config
+                .getint(&section, "TouchScreen")
+                .unwrap()
+                .map(|v| v as i32);
+            device_configuration.keyboard = config
+                .getint(&section, "Keyboard")
+                .unwrap()
+                .map(|v| v as i32);
+            device_configuration.navigation = config
+                .getint(&section, "Navigation")
+                .unwrap()
+                .map(|v| v as i32);
+            device_configuration.screen_layout = config
+                .getint(&section, "ScreenLayout")
+                .unwrap()
+                .map(|v| v as i32);
+            device_configuration.has_hard_keyboard =
+                config.getbool(&section, "HasHardKeyboard").unwrap();
+            device_configuration.has_five_way_navigation =
+                config.getbool(&section, "HasFiveWayNavigation").unwrap();
+            device_configuration.screen_density = config
+                .getint(&section, "Screen.Density")
+                .unwrap()
+                .map(|v| v as i32);
+            device_configuration.gl_es_version = config
+                .getint(&section, "GL.Version")
+                .unwrap()
+                .map(|v| v as i32);
+            device_configuration.system_shared_library = config
+                .get(&section, "SharedLibraries")
+                .unwrap()
+                .split(",")
+                .map(|s| String::from(s))
+                .collect();
+            device_configuration.system_available_feature = config
+                .get(&section, "Features")
+                .unwrap()
+                .split(",")
+                .map(|s| String::from(s))
+                .collect();
+            device_configuration.native_platform = config
+                .get(&section, "Platforms")
+                .unwrap_or_default()
+                .split(",")
+                .map(|s| String::from(s))
+                .collect();
+            device_configuration.screen_width = config
+                .getint(&section, "Screen.Width")
+                .unwrap()
+                .map(|v| v as i32);
+            device_configuration.screen_height = config
+                .getint(&section, "Screen.Height")
+                .unwrap()
+                .map(|v| v as i32);
+            device_configuration.system_supported_locale = config
+                .get(&section, "Locales")
+                .unwrap()
+                .split(",")
+                .map(|s| String::from(s))
+                .collect();
+            device_configuration.gl_extension = config
+                .get(&section, "GL.Extensions")
+                .unwrap()
+                .split(",")
+                .map(|s| String::from(s))
+                .collect();
+            device_configuration.device_feature = config
+                .get(&section, "Features")
+                .unwrap()
+                .split(",")
+                .map(|s| {
+                    let feature_name = String::from(s);
+                    let mut device_feature = DeviceFeature::default();
+                    device_feature.name = Some(feature_name);
+                    device_feature.value = Some(0);
+                    device_feature
+                })
+                .collect();
+            
+            let device_props = DeviceProperties{
+                device_configuration: device_configuration,
+                android_checkin: android_checkin,
+                extra_info: extra_info
+            };
+
+            return device_props;
+        }
+    }
+
+    panic!("Can't find device codename '{}' in {}", device_codename_str, device_properties_path.display());
+}
 
 
 #[cfg(test)]
@@ -1187,7 +1342,7 @@ mod tests {
                 let mut api = Gpapi::new("px_7a", &email);
                 api.set_aas_token(aas_token);
                 if api.login().await.is_ok() {
-                    assert!(api.download("com.instagram.android", None, true, true, &Path::new("/tmp/testing"), None).await.is_ok());
+                    assert!(api.download("com.instagram.android", None, true, true, true, &Path::new("/tmp/testing"), None).await.is_ok());
                 }
             }
         }
